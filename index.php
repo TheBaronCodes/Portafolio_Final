e38c2352dd3fe40068e855d1794a1e8640cf0795
TheBaronCodes <72588168+TheBaronCodes@users.noreply.github.com>
2020-12-02 02:39:02 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Learning Capsule</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <h1 class="h1">Learning Capsule</h1>
+        
+        <div class="box-menu">
+            <a href="/Portafolio_Final/web_gratis/html_gratis/introduccionHTML.php"><img src="images/progra1.jpg" class="link-to"></a>
+            <div class="title-op">Cursos Gratis</div>
+        </div>
+        <div class="box-menu">
+            <a href="/Portafolio_Final/registro.php"><img src="images/progra2.jpg" class="link-to"></a>
+            <div class="title-op">Cursos Avanzados</div>
+        </div>
+    </body>
+</html>
