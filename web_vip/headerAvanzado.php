6bba0291ca81e5ec971e85906e298fa50c5d0c82
TheBaronCodes <72588168+TheBaronCodes@users.noreply.github.com>
2020-12-02 03:23:06 -0300

    Update headerAvanzado.php

--- web_vip/headerAvanzado.php
+++ web_vip/headerAvanzado.php
@@ -9,11 +9,11 @@
     </head>
     <body>
         <div class="menu-bar">
-            <a href="/Portafolio_Final/index.php"><div class="menu-op"><p>INICIO</p></div></a>
-            <a href="/Portafolio_Final/web_vip/html_vip/introHTML_vip.php"><div class="menu-op"><p>HTML</p></div></a>
-            <a href="/Portafolio_Final/web_vip/css_vip/introCSS_vip.php"><div class="menu-op"><p>CSS</p></div></a>
-            <a href="/Portafolio_Final/web_vip/js_vip/introJS_vip.php"><div class="menu-op"><p>JS</p></div></a>
-            <a href="/Portafolio_Final/web_vip/jq_vip/introJQ_vip.php"><div class="menu-op"><p>JQUERY</p></div></a>
+            <a href="/index.php"><div class="menu-op"><p>INICIO</p></div></a>
+            <a href="/web_vip/html_vip/introHTML_vip.php"><div class="menu-op"><p>HTML</p></div></a>
+            <a href="/web_vip/css_vip/introCSS_vip.php"><div class="menu-op"><p>CSS</p></div></a>
+            <a href="/web_vip/js_vip/introJS_vip.php"><div class="menu-op"><p>JS</p></div></a>
+            <a href="/web_vip/jq_vip/introJQ_vip.php"><div class="menu-op"><p>JQUERY</p></div></a>
             <a href="../../registro.php"><div class="menu-op"><p>CONTACTO</p></div></a>
         </div>
     </body>
